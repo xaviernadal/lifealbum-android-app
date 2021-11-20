2eb0f4ff07c67aab5ad67220dd43cb1d268d53e3
Xavier Nadal <xavinadal14@gmail.com>
2021-11-20 03:18:49 +0100

    Database (i guess)

---
+++ app/src/main/java/xaviernadalreales/com/lifealbum/dao/NoteDao.kt
@@ -0,0 +1,16 @@
+package xaviernadalreales.com.lifealbum.dao
+
+import androidx.room.*
+import xaviernadalreales.com.lifealbum.entities.Note
+
+@Dao
+interface NoteDao {
+    @Query("SELECT * FROM notes ORDER BY id DESC")
+    fun getAllNotes() : List<Note>
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insertNote(note : Note)
+
+    @Delete
+    fun deleteNote(note : Note)
+}
---
+++ app/src/main/java/xaviernadalreales/com/lifealbum/database/NotesDatabase.kt
@@ -0,0 +1,28 @@
+package xaviernadalreales.com.lifealbum.database
+
+import android.content.Context
+import androidx.room.Database
+import xaviernadalreales.com.lifealbum.entities.Note
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import xaviernadalreales.com.lifealbum.dao.NoteDao
+
+
+@Database(entities = [Note::class], version = 1, exportSchema = false)
+abstract class NotesDatabase : RoomDatabase() {
+
+    private var notesDatabase: NotesDatabase? = null
+
+    @Synchronized
+    fun getDatabase(context: Context): NotesDatabase? {
+        if (notesDatabase == null) {
+            notesDatabase = Room.databaseBuilder(
+                context,
+                NotesDatabase::class.java,
+                "notes_db"
+            ).build()
+        }
+        return notesDatabase
+    }
+    abstract fun noteDao(): NoteDao
+}
