cd778ffa2f9dad0c8f795a91bc91c1cdd01fba95
Xavier Nadal <xavinadal14@gmail.com>
2021-11-20 03:19:09 +0100

    Database (i guess)

---
+++ app/src/main/java/xaviernadalreales/com/lifealbum/entities/Note.kt
@@ -0,0 +1,15 @@
+package xaviernadalreales.com.lifealbum.entities
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "notes")
+data class Note (
+    @PrimaryKey(autoGenerate = true) val id: Int,
+    @ColumnInfo(name="title") val title: String?,
+    @ColumnInfo(name="noteText") val noteText : String?,
+    @ColumnInfo(name="image_path") val imagePath : String?,
+    @ColumnInfo(name="color") val colorNote : String?,
+    @ColumnInfo(name="webLink") val webLink : String?
+)
