d8bd038fce7492017d233575592520e14ee328b8
xaviernadal <xavinadal14@gmail.com>
2021-11-27 00:14:06 +0100

    Input images in the note, request permits to access gallery, save image path, and display them in the feed of notes (with rounded corners). Still unsure about de lower menu on the note creation activity.

--- app/src/main/java/xaviernadalreales/com/lifealbum/activities/CreateNoteActivity.kt
+++ app/src/main/java/xaviernadalreales/com/lifealbum/activities/CreateNoteActivity.kt
@@ -1,6 +1,13 @@
 package xaviernadalreales.com.lifealbum.activities
 
+import android.Manifest.*
+import android.app.Activity
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.database.Cursor
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
@@ -9,13 +16,21 @@ import xaviernadalreales.com.lifealbum.entities.Note
 import java.text.SimpleDateFormat
 import java.util.*
 import android.os.Looper
+import android.provider.MediaStore
 import android.util.Log
 import android.view.View
 import android.widget.*
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.app.ActivityCompat
+import androidx.core.app.ActivityCompat.requestPermissions
+import androidx.core.app.ActivityCompat.startActivityForResult
+import androidx.core.content.ContextCompat
 import com.google.android.material.bottomsheet.BottomSheetBehavior
 import org.w3c.dom.Text
 import xaviernadalreales.com.lifealbum.database.NotesDatabase
+import java.io.InputStream
 import java.util.concurrent.Executors
+import java.util.jar.Manifest
 
 
 class CreateNoteActivity : AppCompatActivity() {
@@ -23,9 +38,15 @@ class CreateNoteActivity : AppCompatActivity() {
     private lateinit var inputNoteTitle: EditText;
     private lateinit var inputNoteText: EditText
     private lateinit var date: TextView
+    private lateinit var imageNote: ImageView
 
     private lateinit var selectedNoteColor: String
+    private lateinit var selectedImagePath: String
 
+    companion object {
+        private val REQUESTCODE_PERMISSION = 1
+        private val REQUESTCODE_IMAGE = 2
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -39,12 +60,14 @@ class CreateNoteActivity : AppCompatActivity() {
         date = findViewById(R.id.date)
         date.text =
             SimpleDateFormat("EEEE, dd MMMM yyyy HH:mm a", Locale.getDefault()).format(Date())
+        imageNote = findViewById(R.id.imageNote)
 
         val imageSave: ImageView = findViewById(R.id.saveNote)
         imageSave.setOnClickListener { saveNote(); }
 
         //TODO: Canviar pq es fake
         selectedNoteColor = "#333333"
+        selectedImagePath = ""
 
         initColors()
     }
@@ -63,6 +86,7 @@ class CreateNoteActivity : AppCompatActivity() {
         note.noteText = inputNoteText.text.toString()
         note.date = date.text.toString()
         note.colorNote = selectedNoteColor
+        note.imagePath = selectedImagePath
 
         val executor = Executors.newSingleThreadExecutor()
         val handler = Handler(Looper.getMainLooper())
@@ -102,30 +126,116 @@ class CreateNoteActivity : AppCompatActivity() {
         layoutColors.findViewById<View>(R.id.viewColor1).setOnClickListener {
             selectedNoteColor = "#333333"
             imageColor1.setImageResource(R.drawable.ic_done); imageColor2.setImageResource(0)
-            imageColor3.setImageResource(0); imageColor4.setImageResource(0); imageColor5.setImageResource(0)
+            imageColor3.setImageResource(0); imageColor4.setImageResource(0); imageColor5.setImageResource(
+            0
+        )
         }
         layoutColors.findViewById<View>(R.id.viewColor2).setOnClickListener {
             selectedNoteColor = "#FDBE3B"
             imageColor1.setImageResource(0); imageColor2.setImageResource(R.drawable.ic_done)
-            imageColor3.setImageResource(0); imageColor4.setImageResource(0); imageColor5.setImageResource(0)
+            imageColor3.setImageResource(0); imageColor4.setImageResource(0); imageColor5.setImageResource(
+            0
+        )
         }
         layoutColors.findViewById<View>(R.id.viewColor3).setOnClickListener {
             selectedNoteColor = "#FF4842"
             imageColor1.setImageResource(0); imageColor2.setImageResource(0);
-            imageColor3.setImageResource(R.drawable.ic_done); imageColor4.setImageResource(0); imageColor5.setImageResource(0)
+            imageColor3.setImageResource(R.drawable.ic_done); imageColor4.setImageResource(0); imageColor5.setImageResource(
+            0
+        )
         }
         layoutColors.findViewById<View>(R.id.viewColor4).setOnClickListener {
             selectedNoteColor = "#3A52FC"
             imageColor1.setImageResource(0); imageColor2.setImageResource(0);
-            imageColor3.setImageResource(0); imageColor4.setImageResource(R.drawable.ic_done); imageColor5.setImageResource(0)
+            imageColor3.setImageResource(0); imageColor4.setImageResource(R.drawable.ic_done); imageColor5.setImageResource(
+            0
+        )
         }
         layoutColors.findViewById<View>(R.id.viewColor5).setOnClickListener {
             selectedNoteColor = "#000000"
             imageColor1.setImageResource(0); imageColor2.setImageResource(0);
-            imageColor3.setImageResource(0); imageColor4.setImageResource(0); imageColor5.setImageResource(R.drawable.ic_done)
+            imageColor3.setImageResource(0); imageColor4.setImageResource(0); imageColor5.setImageResource(
+            R.drawable.ic_done
+        )
         }
 
+        layoutColors.findViewById<LinearLayout>(R.id.addImage).setOnClickListener {
+            bottomSheetBehavior.state = BottomSheetBehavior.STATE_COLLAPSED
+            when {
+                ContextCompat.checkSelfPermission(
+                    applicationContext,
+                    permission.READ_EXTERNAL_STORAGE
+                ) == PackageManager.PERMISSION_GRANTED -> {
+                    selectImage()
+                }
+                shouldShowRequestPermissionRationale("") -> {
+
+                }
+                else -> {
+                    requestPermissions(
+                        this, arrayOf(
+                            permission.READ_EXTERNAL_STORAGE
+                        ), REQUESTCODE_PERMISSION
+                    )
+                }
+            }
+        }
     }
 
+    val resultLauncher =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+            if (result.resultCode == Activity.RESULT_OK) {
+                val data: Intent? = result.data
+                if (data != null) {
+                    val selectedImageUri: Uri? = data.data
+                    if (selectedImageUri != null) {
+                        try {
+                            val inputStream: InputStream? =
+                                contentResolver.openInputStream(selectedImageUri)
+                            val bitmap: Bitmap = BitmapFactory.decodeStream(inputStream)
+                            imageNote.setImageBitmap(bitmap)
+                            imageNote.visibility = View.VISIBLE
 
+                            selectedImagePath = getPathFromUri(selectedImageUri)
+
+                        } catch (e: Exception) {
+                            Toast.makeText(this, e.message, Toast.LENGTH_SHORT).show()
+                        }
+                    }
+                }
+            }
+        }
+
+    private fun selectImage() {
+        val intent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+        resultLauncher.launch(intent)
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == REQUESTCODE_PERMISSION && grantResults.isNotEmpty()) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                selectImage()
+            } else {
+                Toast.makeText(this, "Permission denied!", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+    private fun getPathFromUri(contentUri : Uri): String {
+        val filePath : String
+        val cursor : Cursor? = contentResolver.query(contentUri, null, null,null, null, null)
+        if (cursor == null) {
+            filePath = contentUri.path.toString()
+        } else {
+            cursor.moveToFirst()
+            val index : Int = cursor.getColumnIndex("_data")
+            filePath = cursor.getString(index)
+            cursor.close()
+        }
+        return filePath
+    }
 }
--- app/src/main/java/xaviernadalreales/com/lifealbum/adapters/NotesAdapter.kt
+++ app/src/main/java/xaviernadalreales/com/lifealbum/adapters/NotesAdapter.kt
@@ -1,11 +1,14 @@
 package xaviernadalreales.com.lifealbum.adapters
 
 
+import android.graphics.BitmapFactory
 import android.graphics.Color.parseColor
 import android.graphics.drawable.GradientDrawable
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.annotation.NonNull
@@ -26,11 +29,13 @@ class NotesAdapter : RecyclerView.Adapter<NotesAdapter.NoteViewHolder> {
         var textTitle: TextView = itemView.findViewById(R.id.textTitle)
         var textDate: TextView = itemView.findViewById(R.id.textDate)
         var layoutNote: LinearLayout = itemView.findViewById(R.id.layoutNote)
+        var imageNote: ImageView = itemView.findViewById(R.id.imageNote)
 
         fun setNote(note: Note) {
             textTitle.text = note.title
             if (textTitle.text == "") textTitle.visibility = View.GONE
             textDate.text = note.date
+
 
             val gradientDrawable: GradientDrawable = layoutNote.background as GradientDrawable
             if (note.colorNote != "") {
@@ -39,7 +44,15 @@ class NotesAdapter : RecyclerView.Adapter<NotesAdapter.NoteViewHolder> {
                 //TODO: Also change this default color haha
                 gradientDrawable.setColor(parseColor("#333333"))
             }
-
+            Log.d("arribe aqui","si")
+            Log.d("i aqui?", "a" + note.imagePath)
+            if (note.imagePath != "") {
+                Log.d("arribe aqui","jajajajaajja")
+                imageNote.setImageBitmap(BitmapFactory.decodeFile(note.imagePath))
+                imageNote.visibility = View.VISIBLE
+            } else {
+                imageNote.visibility = View.GONE
+            }
         }
     }
 
@@ -62,5 +75,6 @@ class NotesAdapter : RecyclerView.Adapter<NotesAdapter.NoteViewHolder> {
     override fun getItemViewType(position: Int): Int {
         return position
     }
+
 
 }
